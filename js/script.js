799e20059da753c7b076de3b2cbd365571463c5b
elena <zianielena@gmail.com>
2024-10-21 18:10:53 +0200

    feat: favourite and tbr caroussel

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
+function toggleProfileMenu() {
+    document.getElementById('profileMenu').classList.toggle('show-menu');
+}
+
+// Fermer le menu si on clique ailleurs
+window.addEventListener('click', function(event) {
+    const profileMenu = document.getElementById('profileMenu');
+    if (!event.target.closest('.icon-profil-nav')) {
+        profileMenu.classList.remove('show-menu');
+    }
+});
+
 function toggleMenu() {
     const menuContainer = document.getElementById('menuContainer');
 
     if (menuContainer.style.display === 'block') {
-        menuContainer.style.display = 'none'; 
+        menuContainer.style.display = 'none';
     } else {
         menuContainer.style.display = 'block';
     }
@@ -14,7 +26,7 @@ function closeMenu(event) {
     const menuBurger = document.getElementById('menuBurger');
 
     if (!menuContainer.contains(event.target) && !menuBurger.contains(event.target)) {
-        menuContainer.style.display = 'none'; 
+        menuContainer.style.display = 'none';
     }
 }
 
