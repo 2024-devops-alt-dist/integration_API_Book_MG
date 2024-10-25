2db8f577885204e712dbc86b7caac416f144a707
maeva <maeva@juriactes.fr>
2024-10-25 15:04:40 +0200

    feature : page detail js

--- js/page_detail.js
+++ js/page_detail.js
@@ -1,7 +1,12 @@
 import { fetchApiBookById } from "./api_book.js";
 
 // DOM SELECTOR
+const imgCouv = document.querySelector('.img-couv')
+const imgCouvMobile = document.querySelector('.img-mobile-psy')
 const h1 = document.getElementById("title-details");
+const author = document.querySelector(".author")
+const publishedDate = document.querySelector('.date')
+const synopsis = document.querySelector('.synopsis')
 
 const displayBookDetails = () => {
   const paramId = new URLSearchParams(window.location.search).get("id");
@@ -10,7 +15,17 @@ const displayBookDetails = () => {
 
     fetchApiBookById(paramId).then((data) => {
       const book = data.volumeInfo;
+      console.log(book)
+      if(book.imageLinks){
+        imgCouv.src = book.imageLinks.large
+        imgCouvMobile.src = book.imageLinks.large
+      }
       h1.textContent = book.title;
+      for (let element of book.authors) {
+       author.textContent = "par " + element
+      }
+      publishedDate.textContent = book.publishedDate
+      synopsis.textContent = book.description
     });
   } catch (error) {
     console.log(error);
