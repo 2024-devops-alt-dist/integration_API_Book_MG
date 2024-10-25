7d1f42a353a2332d5e341872f3c0a7215f444afe
Gwen <gwendoline.garlet@live.fr>
2024-10-25 10:11:09 +0200

    feat: création fn book by id

--- js/api_book.js
+++ js/api_book.js
@@ -2,6 +2,23 @@ const API_KEY = "AIzaSyBSWT1H8T-MrWjcBlsI2mSg0oPqr2lxjAA";
 const URL_API_BOOK = `https://www.googleapis.com/books/v1/volumes`;
 
 /**
+ * fetchApiBookById
+ * @param {idBook} idBook
+ * @returns book || null
+ */
+export const fetchApiBookById = async (idBook) => {
+  try {
+    const resq = await fetch(`${URL_API_BOOK}/${idBook}`);
+    const data = await resq.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+/**
  *
  * @param {string} querry
  * @param {json} searchCache
---
+++ js/page_detail.js
@@ -0,0 +1,19 @@
+import { fetchApiBookById } from "./api_book.js";
+
+// DOM SELECTOR
+const h1 = document.getElementById("title-details");
+
+const displayBookDetails = async () => {
+  const paramId = new URLSearchParams(window.location.search).get("id");
+  try {
+    console.log(paramId);
+
+    fetchApiBookById(paramId).then((data) => {
+      const book = data.volumeInfo;
+      h1.textContent = book.title;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+displayBookDetails();
